9fcd14bbb4f0c3deeb080f6450f24f5874ae3941
andrewtdiz <59515127+andrewtdiz@users.noreply.github.com>
2024-11-09 17:49:03 -0600

    heartbeat

--- src/index.ts
+++ src/index.ts
@@ -77,4 +77,8 @@ app.post("/", async (req: Request, res: Response) => {
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
+
+  setInterval(() => {
+    console.log("Heartbeat");
+  }, 1000);
 });
