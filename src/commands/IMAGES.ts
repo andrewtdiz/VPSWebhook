4c52b9518dc3357781ba698c76e52ad587b664b7
andrewtdiz <59515127+andrewtdiz@users.noreply.github.com>
2024-11-10 12:29:50 -0600

    add_images

--- src/commands/IMAGES.ts
+++ src/commands/IMAGES.ts
@@ -1,3 +1,4 @@
 export default [
   "https://media.discordapp.net/attachments/1200222519010009119/1305170151402115092/Designer3.png?ex=67320dec&is=6730bc6c&hm=85661d6810339aafdff3221782c24b21c4e695da4bd296dbc5df90baf0f2792f&=&format=webp&quality=lossless&width=671&height=671",
+  "https://media.discordapp.net/attachments/1200222519010009119/1305170151402115092/Designer3.png?ex=67320dec&is=6730bc6c&hm=85661d6810339aafdff3221782c24b21c4e695da4bd296dbc5df90baf0f2792f&=&format=webp&quality=lossless&width=671&height=671",
 ];
