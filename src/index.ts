bfe830e37acd28bb357edbd65ae0685ca6d3d443
andrewtdiz <59515127+andrewtdiz@users.noreply.github.com>
2024-11-09 20:51:56 -0600

    fix

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 app.use(
   express.json({
-    verify: (req: any, res, buf) => {
+    verify: (req: any, _, buf) => {
       req.rawBody = buf;
     },
   })
